73849b2c5911f1b15bc8b29b349c759e05c63ce7
Yuri Campos <ygsoares20@gmail.com>
2023-05-10 05:45:09 -0300

    basic implementation (VERY WIP)

--- interface.gd
+++ interface.gd
@@ -1,3 +1,31 @@
 extends Control
 
-@onready var one: int = get.node()
+@onready var texto: Label = get_node("background/display/texto")
+#GLOBAL
+var a: int = 0
+var b: int = 0
+
+func _ready() -> void:
+	for button in get_tree().get_nodes_in_group("button"):
+		button.pressed.connect(on_button_pressed.bind(button.name))
+		
+func on_button_pressed(b_name: String):
+	if b_name not in '+-d*r=':
+		if a != 0:
+			b = int(b_name)
+		else:
+			a = int(b_name)
+	elif b_name == '=':
+		texto.text = str(soma(a, b))
+
+func soma(a: int, b: int) -> int:
+	return a + b
+
+func sub(a: int, b: int) -> int:
+	return a - b
+
+func multi(a: int, b: int) -> int:
+	return a * b
+
+func div(a: int, b: int) -> float:
+	return a / b
